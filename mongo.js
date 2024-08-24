ba151fb3292bf173f9e1173604f290858272a49e
Eduardo D Mori <emori1968@gmail.com>
2024-08-24 14:56:26 -0300

    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('Give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://emori1968:${password}@cluster0.it4ey.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+if (process.argv.length==3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+    process.exit(1)
+  })
+} else
+{
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+  
+  person.save().then(result => {
+    console.log('added', person.name, person.number, 'to phonebook')
+    mongoose.connection.close()
+  })
+
+}
